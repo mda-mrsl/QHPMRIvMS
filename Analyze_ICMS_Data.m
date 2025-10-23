e235e2caf9e46480b12e3275f10e2137c36d1fe3
mda-mrsl <jbankson@mdanderson.org>
2025-10-23 11:36:20 -0500

    Update

--- Analyze_ICMS_Data.m
+++ Analyze_ICMS_Data.m
@@ -2,11 +2,10 @@
 clear
 load('data/MassSpecData.mat')
 
-MediaFlag=0; % Do not include ICMS data from media
-
-for CellLine=1:3
+parfor CellLine=1:3
     MSPKSweep(CellLine,MassSpecData);
 end
+
 
 
 
